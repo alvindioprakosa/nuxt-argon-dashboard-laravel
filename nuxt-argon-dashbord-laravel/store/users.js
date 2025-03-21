5c3d1c360bd488118eedca52b1e9af018f5aef71
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-21 10:11:52 +0700

    Update Users

--- nuxt-argon-dashbord-laravel/store/users.js
+++ nuxt-argon-dashbord-laravel/store/users.js
@@ -1,10 +1,10 @@
-import service from '../services/users-service';
+import service from "../services/users-service";
 
 export const state = () => ({
   list: {},
   user: {},
   meta: {},
-  url: null
+  url: null,
 });
 
 export const mutations = {
@@ -19,49 +19,49 @@ export const mutations = {
   },
   SET_URL: (state, url) => {
     state.url = url;
-  }
+  },
 };
 
 export const actions = {
-  list({commit, dispatch}, params) {
-    return service.list(params, this.$axios)
-      .then(({list, meta}) => {
-        commit('SET_LIST', list);
-        commit('SET_META', meta);
-      });
+  list({ commit, dispatch }, params) {
+    return service.list(params, this.$axios).then(({ list, meta }) => {
+      commit("SET_LIST", list);
+      commit("SET_META", meta);
+    });
   },
 
-  get({commit, dispatch}, params) {
-    return service.get(params, this.$axios)
-      .then((user) => { commit('SET_RESOURCE', user); });
+  get({ commit, dispatch }, params) {
+    return service.get(params, this.$axios).then((user) => {
+      commit("SET_RESOURCE", user);
+    });
   },
 
-  add({commit, dispatch}, params) {
-    return service.add(params, this.$axios)
-      .then((user) => { commit('SET_RESOURCE', user); });
+  add({ commit, dispatch }, params) {
+    return service.add(params, this.$axios).then((user) => {
+      commit("SET_RESOURCE", user);
+    });
   },
 
-  update({commit, dispatch}, params) {
-    return service.update(params, this.$axios)
-      .then((user) => { commit('SET_RESOURCE', user); });
+  update({ commit, dispatch }, params) {
+    return service.update(params, this.$axios).then((user) => {
+      commit("SET_RESOURCE", user);
+    });
   },
 
-  destroy({commit, dispatch}, params) {
+  destroy({ commit, dispatch }, params) {
     return service.destroy(params), this.$axios;
   },
 
-  upload({commit, dispatch}, {user, image}) {
-    return service.upload(user, image, this.$axios)
-      .then((url) => {
-        commit('SET_URL', url);
-      });
-  }
+  upload({ commit, dispatch }, { user, image }) {
+    return service.upload(user, image, this.$axios).then((url) => {
+      commit("SET_URL", url);
+    });
+  },
 };
 
 export const getters = {
-  list: state => state.list,
-  listTotal: state => state.meta.page.total,
-  user: state => state.user,
-  url: state => state.url
+  list: (state) => state.list,
+  listTotal: (state) => state.meta.page.total,
+  user: (state) => state.user,
+  url: (state) => state.url,
 };
-
