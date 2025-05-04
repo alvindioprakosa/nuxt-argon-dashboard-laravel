0f319e64b36d9d91410d49e7933e8b98854ca604
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-04 23:20:01 +0700

    Update Content Footer

--- nuxt-argon-dashbord-laravel/components/layouts/argon/ContentFooter.vue
+++ nuxt-argon-dashbord-laravel/components/layouts/argon/ContentFooter.vue
@@ -3,83 +3,74 @@
     <div class="row align-items-center justify-content-lg-between">
       <div class="col-lg-6">
         <ul class="nav nav-footer">
-          <li class="nav-item">
+          <li
+            v-for="(link, index) in footerLinks"
+            :key="index"
+            class="nav-item"
+          >
             <a
-              href="https://www.creative-tim.com"
+              :href="link.href"
               class="nav-link"
               target="_blank"
               rel="noopener"
-              >CREATIVE TIM</a
             >
-          </li>
-          <li class="nav-item">
-            <a
-              href="https://www.updivision.com"
-              class="nav-link"
-              target="_blank"
-              rel="noopener"
-              >UPDIVISION</a
-            >
-          </li>
-          <li class="nav-item">
-            <a
-              href="https://www.creative-tim.com/presentation"
-              class="nav-link"
-              target="_blank"
-              rel="noopener"
-              >ABOUT US</a
-            >
-          </li>
-          <li class="nav-item">
-            <a
-              href="https://blog.creative-tim.com"
-              class="nav-link"
-              target="_blank"
-              rel="noopener"
-              >BLOG</a
-            >
-          </li>
-          <li class="nav-item">
-            <a
-              href="https://www.creative-tim.com/license"
-              class="nav-link"
-              target="_blank"
-              rel="noopener"
-              >LICENSES</a
-            >
+              {{ link.label }}
+            </a>
           </li>
         </ul>
       </div>
 
       <div class="col-lg-6">
         <div class="copyright text-center text-lg-right text-muted">
-          © {{ year }}, made with <i class="fa fa-heart heart"></i> by<a
+          © {{ year }}, made with <i class="fa fa-heart heart"></i> by
+          <a
             href="https://www.creative-tim.com"
             class="font-weight-bold ml-1"
             target="_blank"
             rel="noopener"
-            >Creative Tim</a
           >
-          and<a
+            Creative Tim
+          </a>
+          and
+          <a
             href="https://www.updivision.com"
             class="font-weight-bold ml-1"
             target="_blank"
             rel="noopener"
-            >UPDIVISION</a
           >
+            UPDIVISION
+          </a>
           for a better web.
         </div>
       </div>
     </div>
   </footer>
 </template>
+
 <script>
 export default {
+  name: "AppFooter",
   data() {
     return {
       year: new Date().getFullYear(),
+      footerLinks: [
+        { label: "CREATIVE TIM", href: "https://www.creative-tim.com" },
+        { label: "UPDIVISION", href: "https://www.updivision.com" },
+        { label: "ABOUT US", href: "https://www.creative-tim.com/presentation" },
+        { label: "BLOG", href: "https://blog.creative-tim.com" },
+        { label: "LICENSES", href: "https://www.creative-tim.com/license" },
+      ],
     };
   },
 };
 </script>
-<style></style>
+
+<style scoped>
+.footer {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+.heart {
+  color: red;
+}
+</style>
