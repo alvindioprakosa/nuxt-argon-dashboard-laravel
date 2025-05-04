5acf11fc63ab576e50487facd833748d4ccec622
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-04 23:20:16 +0700

    Update User Edit Card

--- nuxt-argon-dashbord-laravel/components/Dashboard/Cards/UserEditCard.vue
+++ nuxt-argon-dashbord-laravel/components/Dashboard/Cards/UserEditCard.vue
@@ -4,7 +4,7 @@
       <h1>Edit Profile</h1>
     </div>
     <div class="card-body">
-      <form ref="profile_form" @submit.prevent="handleProfileUpdate">
+      <form @submit.prevent="handleProfileUpdate">
         <base-input
           label="Name"
           prepend-icon="fas fa-user"
@@ -12,6 +12,7 @@
           v-model="user.name"
         />
         <validation-error :errors="apiValidationErrors.name" />
+
         <base-input
           label="Email"
           prepend-icon="fas fa-envelope"
@@ -19,12 +20,9 @@
           v-model="user.email"
         />
         <validation-error :errors="apiValidationErrors.email" />
+
         <div class="my-4">
-          <base-button
-            type="button"
-            class="btn btn-sm btn-primary"
-            native-type="submit"
-          >
+          <base-button class="btn btn-sm btn-primary" native-type="submit">
             Submit
           </base-button>
         </div>
@@ -32,11 +30,12 @@
     </div>
   </div>
 </template>
+
 <script>
 import BaseInput from "~/components/argon-core/Inputs/BaseInput.vue";
 import BaseButton from "~/components/argon-core/BaseButton.vue";
-import formMixin from "@/mixins/form-mixin";
 import ValidationError from "~/components/ValidationError.vue";
+import formMixin from "@/mixins/form-mixin";
 
 export default {
   name: "UserEditCard",
@@ -50,18 +49,22 @@ export default {
   mixins: [formMixin],
 
   props: {
-    user: Object,
+    user: {
+      type: Object,
+      required: true,
+    },
   },
 
   methods: {
     async handleProfileUpdate() {
-      if (["1"].includes(this.user.id)) {
-        await this.$notify({
+      if (parseInt(this.user.id) === 1) {
+        this.$notify({
           type: "danger",
-          message: "You are not allowed not change data of default users.",
+          message: "You are not allowed to change data of default users.",
         });
         return;
       }
+
       try {
         await this.$store.dispatch("profile/update", this.user);
         this.unsetApiValidationErrors();
@@ -70,13 +73,15 @@ export default {
           type: "success",
           message: "Profile updated successfully.",
         });
-        await this.$store.getters["profile/me"];
+
+        await this.$store.dispatch("profile/me");
       } catch (error) {
         this.$notify({
           type: "danger",
           message: "Oops, something went wrong!",
         });
-        this.setApiValidation(error.response.data.errors);
+
+        this.setApiValidation(error.response?.data?.errors || {});
       }
     },
   },
